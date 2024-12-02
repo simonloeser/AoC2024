b1c36f77881b91bb45c3985dedab1ff122452cbc
simonloeser <92864888+simonloeser@users.noreply.github.com>
2024-12-02 16:27:25 +0100

    feat: add day02 part2

--- src/day02/day02.ts
+++ src/day02/day02.ts
@@ -24,19 +24,34 @@ function isSafeReport(report: number[]): boolean {
     return true;
 }
 
-function countSafeReports(filePath: string) {
+function countSafeReports(filePath: string, part: number) {
     const fileContent = fs.readFileSync(filePath, 'utf-8');
     const lines = fileContent.trim().split('\n');
     let safeReports = 0;
 
     for (const line of lines) {
         const report = line.split(/\s+/).map(Number);
-        if (isSafeReport(report)) {
+        if (part == 1 ? isSafeReport(report) : isSafeWithProblemDampener(report)) {
             safeReports++;
         }
     }
     return safeReports;
 }
 
+function isSafeWithProblemDampener(report: number[]): boolean {
+    if (isSafeReport(report)) {
+        return true;
+    }
+
+    for (let i = 0; i < report.length; i++) {
+        const modifiedReport = report.slice(0, i).concat(report.slice(i + 1));
+        if (isSafeReport(modifiedReport)) {
+            return true;
+        }
+    }
+    return false;
+}
+
 const filePath = 'day02.txt';
-console.log(`[PART 01] Number of safe reports: ${countSafeReports(filePath)}`)
+console.log(`[PART 01] Number of safe reports: ${countSafeReports(filePath, 1)}`);
+console.log(`[PART 02] Number of safe reports with Problem Dampener: ${countSafeReports(filePath, 2)}`);
